9c283a2662aae318d9e5d3ae902d441dfe2b57f1
Md. Hasanur Rahman <hasanurhasu127@gmail.com>
2023-12-28 21:42:49 +0600

    update banner

--- src/pages/Banner/Banner.jsx
+++ src/pages/Banner/Banner.jsx
@@ -1,4 +1,4 @@
-
+import React, { useRef, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -9,31 +9,50 @@ import 'swiper/css/navigation';
 
 import './Banner.css';
 
+import img1 from '../../assets/books/book2.jpg';
+
 // import required modules
 import { Pagination, Navigation } from 'swiper/modules';
 
+const Banner = () => {
 
-    const Banner = () => {
-        return (
-            <div className='mt-24'>
-                <Swiper
-                    slidesPerView={3}
-                    centeredSlides={true}
-                    spaceBetween={30}
-                    pagination={{
-                        type: 'fraction',
-                    }}
-                    navigation={true}
-                    modules={[Pagination, Navigation]}
-                    className="mySwiper"
-                >
-                    <SwiperSlide><img src="../../assets/books/book2.jpg" alt="" /></SwiperSlide>
-                    <SwiperSlide>Slide 2</SwiperSlide>
-                    <SwiperSlide>Slide 3</SwiperSlide>
-                    <SwiperSlide>Slide 4</SwiperSlide>
-                </Swiper>
-            </div>
-        );
-    };
+    return (
+        <div className='mt-24'>
+            <Swiper
+                pagination={{
+                    type: 'fraction',
+                }}
+                navigation={true}
+                modules={[Pagination, Navigation]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <div className='px-4 lg:px-24 flex w-full justify-between items-center'>
+                        <div className="card">
+                            <div className="card-body">
+                                <h2 className="card-title">Card title!</h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions justify-end">
+                                    <button className="btn btn-primary">Buy Now</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <img src={img1} alt="" />
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                <SwiperSlide>Slide 5</SwiperSlide>
+                <SwiperSlide>Slide 6</SwiperSlide>
+                <SwiperSlide>Slide 7</SwiperSlide>
+                <SwiperSlide>Slide 8</SwiperSlide>
+                <SwiperSlide>Slide 9</SwiperSlide>
+            </Swiper>
+        </div>
+    );
+};
 
-    export default Banner;
+export default Banner;
